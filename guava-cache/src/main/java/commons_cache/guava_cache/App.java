3963366ed34b3abfed940944df4623dabe4bc0e2
zhangtian <zhangtian@DESKTOP-JF9M0VD>
2016-07-21 16:15:16 +0800

    Guava缓存

---
+++ guava-cache/src/main/java/commons_cache/guava_cache/App.java
@@ -0,0 +1,13 @@
+package commons_cache.guava_cache;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
