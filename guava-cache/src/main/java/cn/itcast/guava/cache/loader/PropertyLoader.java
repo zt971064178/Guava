337d931db71b2156d9baa2793eebe66b64a06e2b
zhangtian <zhangtian@happy>
2016-07-23 13:43:03 +0800

    配置文件管理

---
+++ guava-cache/src/main/java/cn/itcast/guava/cache/loader/PropertyLoader.java
@@ -0,0 +1,82 @@
+package cn.itcast.guava.cache.loader;
+
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Properties;
+
+import cn.itcast.guava.cache.annotation.PropertyConfigiration;
+import cn.itcast.guava.cache.annotation.Value;
+import cn.itcast.guava.cache.utils.PropertiesUtils;
+
+/**
+ * ClassName: PropertyLoader  
+ * (属性加载器)
+ * @author zhangtian  
+ * @version
+ */
+public class PropertyLoader {
+	public static Object loadProperty(Class<?> clazz) {
+		try {
+			Object object = clazz.newInstance() ;
+			if(clazz.isAnnotationPresent(PropertyConfigiration.class)) {
+				// 获取类注解
+				PropertyConfigiration propertyConfigiration = clazz.getAnnotation(PropertyConfigiration.class) ;
+				String location = propertyConfigiration.location() ;
+				if(location.equals("")) {
+					throw new RuntimeException("guava-cache.properties配置文件无法找到......") ;
+				} else {
+					Properties properties = PropertiesUtils.loadProperties(location) ;
+					if(properties == null)
+						return null ;
+						
+					Field[] fields = clazz.getDeclaredFields() ;
+					for(Field field : fields) {
+						if(field.isAnnotationPresent(Value.class)){
+							Value value = field.getAnnotation(Value.class) ;
+							field.setAccessible(true);
+							String v = value.value() ;
+							String propertyVallue = properties.getProperty(v.substring(2, v.length()-1)) ;
+							if(v.startsWith("${") && v.endsWith("}")) {
+								if(field.getType().isPrimitive()) {
+									if(field.getType().isAssignableFrom(int.class)) {
+										setProperty(object, field.getName(), Integer.parseInt(propertyVallue));
+									} else if(field.getType().isAssignableFrom(boolean.class)) {
+										setProperty(object, field.getName(), Boolean.parseBoolean(propertyVallue));
+									} else if(field.getType().isAssignableFrom(long.class)) {
+										setProperty(object, field.getName(), Long.parseLong(propertyVallue));
+									}
+									else {
+										throw new RuntimeException("类型转换异常......") ;
+									}
+								}
+							} else {
+								throw new RuntimeException("数据格式错误......") ;
+							}
+						}
+					}
+				}
+				return object ;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	/**
+	 * JavaBean内省处理Bean
+	 * @param bean
+	 * @param fieldName
+	 * @param value
+	 * @throws Exception
+	 */
+	private static void setProperty(Object bean, String fieldName, Object value) throws Exception {
+        // 创建属性描述器
+        PropertyDescriptor descriptor = new PropertyDescriptor(fieldName, bean.getClass());
+        // 获得 写方法
+        Method writeMethod = descriptor.getWriteMethod();
+        // 调用 写方法        
+        writeMethod.invoke(bean, value);
+    }
+}
--- guava-cache/src/main/java/cn/itcast/guava/cache/properties/GuavaCacheProperties.java
+++ guava-cache/src/main/java/cn/itcast/guava/cache/properties/GuavaCacheProperties.java
@@ -1,8 +1,128 @@
 package cn.itcast.guava.cache.properties;
 
 import cn.itcast.guava.cache.annotation.PropertyConfigiration;
+import cn.itcast.guava.cache.annotation.Value;
 
 @PropertyConfigiration(location="guava-cache.properties")
 public class GuavaCacheProperties {
+	@Value("${guava.cache.refreshAfterWrite}")
+	private boolean refreshAfterWrite ;
+	@Value("${guava.cache.refreshTimeType}")
+	private String refreshTimeType ;
+	@Value("${guava.cache.refreshTime}")
+	private long refreshTime ;
+	@Value("${guava.cache.expireAfterWrite}")
+	private boolean expireAfterWrite ;
+	@Value("${guava.cache.expireAfterAccess}")
+	private boolean expireAfterAccess ;
+	@Value("${guava.cache.expireTimeType}")
+	private String expireTimeType ;
+	@Value("${guava.cache.expireTime}")
+	private long expireTime ;
+	@Value("${guava.cache.softValues}")
+	private boolean softValues ;
+	@Value("${guava.cache.maximumSize}")
+	private long maximumSize ;
+	@Value("${guava.cache.ticker}")
+	private boolean ticker ;
+	@Value("${guava.cache.concurrencyLevel}")
+	private int concurrencyLevel ;
+	@Value("${guava.cache.stats}")
+	private boolean stats ;
 
+	public boolean isRefreshAfterWrite() {
+		return refreshAfterWrite;
+	}
+
+	public void setRefreshAfterWrite(boolean refreshAfterWrite) {
+		this.refreshAfterWrite = refreshAfterWrite;
+	}
+
+	public String getRefreshTimeType() {
+		return refreshTimeType;
+	}
+
+	public void setRefreshTimeType(String refreshTimeType) {
+		this.refreshTimeType = refreshTimeType;
+	}
+
+	public long getRefreshTime() {
+		return refreshTime;
+	}
+
+	public void setRefreshTime(long refreshTime) {
+		this.refreshTime = refreshTime;
+	}
+
+	public boolean isExpireAfterWrite() {
+		return expireAfterWrite;
+	}
+
+	public void setExpireAfterWrite(boolean expireAfterWrite) {
+		this.expireAfterWrite = expireAfterWrite;
+	}
+
+	public boolean isExpireAfterAccess() {
+		return expireAfterAccess;
+	}
+
+	public void setExpireAfterAccess(boolean expireAfterAccess) {
+		this.expireAfterAccess = expireAfterAccess;
+	}
+
+	public String getExpireTimeType() {
+		return expireTimeType;
+	}
+
+	public void setExpireTimeType(String expireTimeType) {
+		this.expireTimeType = expireTimeType;
+	}
+
+	public long getExpireTime() {
+		return expireTime;
+	}
+
+	public void setExpireTime(long expireTime) {
+		this.expireTime = expireTime;
+	}
+
+	public boolean isSoftValues() {
+		return softValues;
+	}
+
+	public void setSoftValues(boolean softValues) {
+		this.softValues = softValues;
+	}
+
+	public long getMaximumSize() {
+		return maximumSize;
+	}
+
+	public void setMaximumSize(long maximumSize) {
+		this.maximumSize = maximumSize;
+	}
+
+	public boolean isTicker() {
+		return ticker;
+	}
+
+	public void setTicker(boolean ticker) {
+		this.ticker = ticker;
+	}
+
+	public int getConcurrencyLevel() {
+		return concurrencyLevel;
+	}
+
+	public void setConcurrencyLevel(int concurrencyLevel) {
+		this.concurrencyLevel = concurrencyLevel;
+	}
+
+	public boolean isStats() {
+		return stats;
+	}
+
+	public void setStats(boolean stats) {
+		this.stats = stats;
+	}
 }
